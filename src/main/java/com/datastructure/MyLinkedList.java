5bba99c82952cfd922991705dada115c164d4397
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-18 17:20:33 +0530

    Added:Ability to Create a Stack

---
+++ src/main/java/com/datastructure/MyLinkedList.java
@@ -0,0 +1,38 @@
+package com.datastructure;
+
+public class MyLinkedList {
+    public INode head;
+    public INode tail;
+
+    public void MyLinkedList(){
+        this.head=null;
+        this.tail=null;
+    }
+    //method to add new node to linked list
+    public void add(INode newNode) {
+        if(this.tail == null) {
+            this.tail=newNode;
+        }
+        if(this.head == null) {
+            this.head=newNode;
+        }
+        else{
+            INode tempNode = this.head;
+            this.head=newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    // method to display node data
+    public void printMyNodes() {
+        StringBuffer myNodes = new StringBuffer("My Nodes : ");
+        INode tempNode = head;
+        while(tempNode.getNext() != null){
+            myNodes.append(tempNode.getKey());
+            if(!tempNode.equals(tail))
+                myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+}
