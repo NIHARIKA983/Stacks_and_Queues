b92d07b4e0224bc667f3b7272e494fb115665e06
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-18 18:31:44 +0530

    Added:Ability to dequeue from the beginning

--- src/test/java/com/datastructure/MyQueueTest.java
+++ src/test/java/com/datastructure/MyQueueTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyQueueTest {
+    //UC3
     @Test
     public void given3Numbers_WhenAddedToQueue_ShouldAddAtEnd(){
         MyNode<Integer> myFirstNode=new MyNode<>(56);
@@ -18,5 +19,19 @@ public class MyQueueTest {
                 myQueue.getHead().getNext().equals(mySecondNode) &&
                 myQueue.getTail().equals(myThirdNode);
         Assert.assertTrue(result);
+    }
+    //UC4
+    @Test
+    public void given3Numbers_WhenDeletedANode_ShouldMatchWith_NodeAtBeginning(){
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        MyQueue myQueue = new MyQueue();
+        myQueue.enQueue(myFirstNode);
+        myQueue.enQueue(mySecondNode);
+        myQueue.enQueue(myThirdNode);
+        INode frontNode = myQueue.deQueue();
+        myQueue.printQueue();
+        Assert.assertEquals(myFirstNode,frontNode);
     }
 }
